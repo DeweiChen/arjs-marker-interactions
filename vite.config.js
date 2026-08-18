9f5f6d52661373f1162d09a446ce0784a64eb116
DW Chen <tsukasa800921@gmail.com>
2026-08-18 16:55:29 +0800

    feat: add vite config and github pages deployment

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+export default defineConfig({
+  base: '/arjs-marker-interactions/',
+  plugins: [
+    basicSsl()
+  ],
+  server: {
+    host: true,
+    port: 5173,
+    https: true
+  },
+  build: {
+    target: 'esnext'
+  }
+});
